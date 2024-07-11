65204a19c8316a8663d68b229a2b621fec2c9a4a
lebozec <nicolas.lebozec@xelians.fr>
2024-07-11 10:43:43 +0200

    Room correction

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import { FaPlus, FaDoorOpen } from "react-icons/fa";
-import { io } from "socket.io-client";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import { useToast } from "@/components/ui/use-toast"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Layout from "@/components/layout/main/LayoutMain";
 import ScoreGame from "./games/ScoreGame";
-import { useToast } from "@/components/ui/use-toast";
 import { useAuth } from "@/provider/authProvider";
 
 const socket = io(import.meta.env.VITE_API_URL);
@@ -33,35 +32,45 @@ const RoomList: React.FC = () => {
   useEffect(() => {
     fetchRooms();
 
-    socket.on("update_rooms", (updatedRooms: Room[]) => {
+    function onUpdateRooms(updatedRooms: Room[]) {
       setRooms(updatedRooms);
+    }
+
+    socket.on('room_created', (data: {room: string, creator_id: string, game_type: string}) => {
+      console.log(data);
+      console.log("JE suis la");
+      if (userInfo._id === data.creator_id) {
+        console.log("user_id : ", userInfo._id);
+        console.log("creator_id : ", data.creator_id);
+        toast({
+          description: `Room ${data.room} a été crée !`,
+        })
+        socket.emit("join_room_" + data.game_type,  { room: data.room, user_id: userInfo._id });
+      }
     });
 
-    socket.on("room_created", ({ room }) => {
-      toast({ title: `Room ${room} created successfully!` });
-      navigate(`/${gameType}/${room}`);
-    });
 
-    socket.on("room_joined", ({ room }) => {
-      toast({ title: `Joined room ${room}` });
-      navigate(`/${gameType}/${room}`);
-    });
 
-    socket.on("error", (error) => {
-      toast({
-        variant: "destructive",
-        title: "Error",
-        description: error.message,
-      });
-    });
+    function onRoomJoined({ room }: { room: Room }) {
+      toast.success(`Joined room ${room.room_name}`);
+      //navigate(`/${gameType}/${room._id}`);
+    }
+
+    function onError(error: { message: string }) {
+      toast.error(error.message);
+    }
+
+    socket.on("update_rooms", onUpdateRooms);
+    //socket.on("room_joined", onRoomJoined);
+    socket.on("error", onError);
 
     return () => {
-      socket.off("update_rooms");
+      socket.off("update_rooms", onUpdateRooms);
       socket.off("room_created");
-      socket.off("room_joined");
-      socket.off("error");
+      //socket.off("room_joined", onRoomJoined);
+      socket.off("error", onError);
     };
-  }, [navigate, toast, gameType]);
+  }, [navigate, gameType]);
 
   const fetchRooms = async () => {
     try {
@@ -104,9 +113,9 @@ const RoomList: React.FC = () => {
     }
   };
 
-  const handleJoin = (roomName: string) => {
+  const handleJoin = (roomId: string) => {
     if (userInfo) {
-      socket.emit("join_room_" + gameType, { room: roomName, user_id: userInfo._id });
+      socket.emit("join_room_" + roomId, { room: roomId, user_id: userInfo._id });
     }
   };
 
@@ -128,68 +137,64 @@ const RoomList: React.FC = () => {
   };
 
   const filteredRooms = rooms.filter((room) =>
-    room.room_name.toLowerCase().includes(searchTerm.toLowerCase())
+      room.room_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  console.log("----FILTERED ROOMS", filteredRooms);
-  
-
   return (
-    <Layout>
-      <div className="container mx-auto py-12 min-h-screen">
-        <h1 className="text-4xl font-bold text-center mb-8">{gameType}</h1>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="flex flex-col max-h-[750px]">
-            <h2 className="text-2xl font-bold mb-4">Create a Room</h2>
-            <div className="flex gap-5 items-center mb-8">
-              <div className="w-1/2">
-                <Input
-                  className="mr-2"
-                  placeholder="Enter room name"
-                  value={newRoomName}
-                  onChange={(e) => setNewRoomName(e.target.value)}
-                />
-              </div>
-              <Button onClick={handleCreateRoom}>
-                <FaPlus className="mr-2" />
-                Create Room
-              </Button>
-            </div>
-            <h2 className="text-2xl font-bold mb-4">Available Rooms</h2>
-            <Input
-              className="mb-4"
-              placeholder="Search rooms"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <ScrollArea className="flex-grow">
-              {filteredRooms.length > 0 ? (
-                <div className="space-y-4">
-                  {filteredRooms.map((room) => (
-                    <div
-                      key={room?._id}
-                      className="block p-4 border rounded-lg shadow-sm hover:bg-muted transition cursor-pointer"
-                    >
-                      <div className="flex justify-between items-center">
-                        <span>{room?.room_name}</span>
-                        {/* <span>{room.playerCount || 0}/2</span> */}
-                        <FaDoorOpen onClick={() => handleJoin(room?.room_name)} />
-                        {room?.creator_id === userInfo?._id && (
-                          <Button onClick={() => handleDelete(room?.room_name)}>Delete</Button>
-                        )}
-                      </div>
-                    </div>
-                  ))}
+      <Layout>
+        <div className="container mx-auto py-12 min-h-screen">
+          <h1 className="text-4xl font-bold text-center mb-8">{gameType}</h1>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <div className="flex flex-col max-h-[750px]">
+              <h2 className="text-2xl font-bold mb-4">Create a Room</h2>
+              <div className="flex gap-5 items-center mb-8">
+                <div className="w-1/2">
+                  <Input
+                      className="mr-2"
+                      placeholder="Enter room name"
+                      value={newRoomName}
+                      onChange={(e) => setNewRoomName(e.target.value)}
+                  />
                 </div>
-              ) : (
-                <p className="flex-grow">No rooms found</p>
-              )}
-            </ScrollArea>
+                <Button onClick={handleCreateRoom}>
+                  <FaPlus className="mr-2" />
+                  Create Room
+                </Button>
+              </div>
+              <h2 className="text-2xl font-bold mb-4">Available Rooms</h2>
+              <Input
+                  className="mb-4"
+                  placeholder="Search rooms"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <ScrollArea className="flex-grow">
+                {filteredRooms.length > 0 ? (
+                    <div className="space-y-4">
+                      {filteredRooms.map((room) => (
+                          <div
+                              key={room?._id}
+                              className="block p-4 border rounded-lg shadow-sm hover:bg-muted transition cursor-pointer"
+                          >
+                            <div className="flex justify-between items-center">
+                              <span>{room?.room_name}</span>
+                              <FaDoorOpen onClick={() => handleJoin(room?._id)} />
+                              {room?.creator_id === userInfo?._id && (
+                                  <Button onClick={() => handleDelete(room?.room_name)}>Delete</Button>
+                              )}
+                            </div>
+                          </div>
+                      ))}
+                    </div>
+                ) : (
+                    <p className="flex-grow">No rooms found</p>
+                )}
+              </ScrollArea>
+            </div>
+            <ScoreGame />
           </div>
-          <ScoreGame />
         </div>
-      </div>
-    </Layout>
+      </Layout>
   );
 };
 
