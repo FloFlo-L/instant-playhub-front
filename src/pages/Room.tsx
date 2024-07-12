cb09b985f99a8e80f9802d3f03d34690380ce03f
FloFlo-L <florian.lescribaa@gmail.com>
2024-07-12 10:26:46 +0200

    fix/h1

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -130,7 +130,7 @@ const RoomList: React.FC = () => {
   return (
     <Layout>
       <div className="container mx-auto py-12 min-h-screen">
-        <h1 className="text-4xl font-bold text-center mb-8">{capitalizeFirstLetter(gameType)}</h1>
+        <h1 className="text-4xl font-bold text-center mb-12">{capitalizeFirstLetter(gameType)}</h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           <div className="flex flex-col max-h-[750px]">
             <h2 className="text-2xl font-bold mb-4">Créer une partie</h2>
