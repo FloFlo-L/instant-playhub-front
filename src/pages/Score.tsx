7777878522fd1666a12a1ee427f5f08d8f3ac2a6
FloFlo-L <florian.lescribaa@gmail.com>
2024-07-12 09:50:44 +0200

    feature/score&share

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -9,6 +9,7 @@ import { Loader2 } from "lucide-react";
 import { useAuth } from "@/provider/authProvider";
 import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "@/components/ui/table"; // Assurez-vous d'importer correctement les composants Table
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Link } from "react-router-dom";
 
 const Score = () => {
     const [scores, setScores] = useState([]);
@@ -17,12 +18,14 @@ const Score = () => {
     const [userLoading, setUserLoading] = useState(false); 
     const [noScores, setNoScores] = useState(false); 
     const { token, userInfo } = useAuth();
+    const [activeTab, setActiveTab] = useState("morpion"); // Nouvel état pour suivre l'onglet actif
 
     useEffect(() => {
         if (userInfo) {
             fetchUserScores(userInfo._id);
+            fetchScores(activeTab); // Charger les scores du premier onglet par défaut
         }
-    }, [userInfo]);
+    }, [userInfo, activeTab]);
 
     const fetchScores = async (gameType) => {
         setLoading(true);
@@ -69,16 +72,21 @@ const Score = () => {
         }
     };
 
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+        fetchScores(tab);
+    };
+
     return (
         <LayoutMain>
             <div className="container mx-auto py-12 min-h-screen">
                 <h1 className="text-3xl mb-6 text-center">Scores</h1>
-                <Tabs defaultValue="morpion" className="">
+                <Tabs defaultValue="morpion" className="" onValueChange={handleTabChange}>
                     <TabsList className="grid w-full grid-cols-4">
-                        <TabsTrigger value="morpion" onClick={() => fetchScores("morpion")}>Morpion</TabsTrigger>
-                        <TabsTrigger value="uno" onClick={() => fetchScores("uno")}>Uno</TabsTrigger>
-                        <TabsTrigger value="breakout" onClick={() => fetchScores("breakout")}>Casse briques</TabsTrigger>
-                        <TabsTrigger value="connect-four" onClick={() => fetchScores("connect-four")}>Puissance 4</TabsTrigger>
+                        <TabsTrigger value="morpion">Morpion</TabsTrigger>
+                        <TabsTrigger value="uno">Uno</TabsTrigger>
+                        <TabsTrigger disabled={true} value="breakout">Casse briques</TabsTrigger>
+                        <TabsTrigger disabled={true} value="connect-four">Puissance 4</TabsTrigger>
                     </TabsList>
                     <TabsContent value="morpion">
                         <h2 className="text-center text-2xl my-3">Mes scores</h2>
@@ -123,8 +131,10 @@ const Score = () => {
                                             </CardContent>
                                             <CardFooter>
                                                 <p className="mr-2">Je partage mon score à mes amis</p>
-                                                <Button size={"icon"}>
-                                                    <FaShareAlt />
+                                                <Button size={"icon"} asChild>
+                                                    <Link target="blank" to={`http://twitter.com/share?text=J'ai actuellement ${userScores.morpion?.score} points sur le jeu morpion. Rejoint moi sur &url=http://localhost:5173/&hashtags=instantPlayHub et essaye de me battre`}>
+                                                        <FaShareAlt />
+                                                    </Link>
                                                 </Button>
                                             </CardFooter>
                                         </Card>
@@ -140,7 +150,7 @@ const Score = () => {
                                 <Loader2 className="mr-2 h-6 w-6 animate-spin" />
                             </div>
                         ) : noScores ? (
-                            <p className="text-center text-xl my-3">Vous n'avez aucun score associé à ce jeu</p>
+                            <p className="text-center text-xl my-3">Aucun score général disponible pour ce jeu</p>
                         ) : (
                             <Table>
                                 <TableHeader>
@@ -175,7 +185,100 @@ const Score = () => {
                         )}
                     </TabsContent>
                     <TabsContent value="uno">
-                        {/* Répétez pour chaque onglet */}
+                        <h2 className="text-center text-2xl my-3">Mes scores</h2>
+                        {userLoading ? (
+                            <div className="flex justify-center items-center w-full">
+                                <Loader2 className="mr-2 h-6 w-6 animate-spin" />
+                            </div>
+                        ) : (
+                            <>
+                                {userScores.uno ? (
+                                    <div className="grid gap-8 w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                                        <Card>
+                                            <CardHeader>
+                                                <CardDescription className="">Nombre de Victoire</CardDescription>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <p className="text-2xl font-bold">{userScores.uno?.wins || 0}</p>
+                                            </CardContent>
+                                        </Card>
+                                        <Card>
+                                            <CardHeader>
+                                                <CardDescription className="">Nombre d'égalité</CardDescription>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <p className="text-2xl font-bold">{userScores.uno?.draws || 0}</p>
+                                            </CardContent>
+                                        </Card>
+                                        <Card>
+                                            <CardHeader>
+                                                <CardDescription className="">Nombre de défaite</CardDescription>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <p className="text-2xl font-bold">{userScores.uno?.losses || 0}</p>
+                                            </CardContent>
+                                        </Card>
+                                        <Card>
+                                            <CardHeader>
+                                                <CardDescription className="">Mon score</CardDescription>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <p className="text-2xl font-bold">{userScores.uno?.score || 0}</p>
+                                            </CardContent>
+                                            <CardFooter>
+                                                <p className="mr-2">Je partage mon score à mes amis</p>
+                                                <Button size={"icon"} asChild>
+                                                    <Link target="blank" to={`http://twitter.com/share?text=J'ai actuellement ${userScores.uno?.score} points sur le jeu uno. Rejoint moi sur &url=http://localhost:5173/&hashtags=instantPlayHub et essaye de me battre`}>
+                                                        <FaShareAlt />
+                                                    </Link>
+                                                </Button>
+                                            </CardFooter>
+                                        </Card>
+                                    </div>
+                                ) : (
+                                    <p className="text-center text-xl my-3">Vous n'avez aucun score associé à ce jeu</p>
+                                )}
+                            </>
+                        )}
+                        <h2 className="text-center text-2xl my-3">Scores généraux</h2>
+                        {loading ? (
+                            <div className="flex justify-center items-center w-full">
+                                <Loader2 className="mr-2 h-6 w-6 animate-spin" />
+                            </div>
+                        ) : noScores ? (
+                            <p className="text-center text-xl my-3">Aucun score général disponible pour ce jeu</p>
+                        ) : (
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>#</TableHead>
+                                        <TableHead>Joueur</TableHead>
+                                        <TableHead>Victoires</TableHead>
+                                        <TableHead>Égalités</TableHead>
+                                        <TableHead>Défaites</TableHead>
+                                        <TableHead>Score</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {scores.map((score, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{index + 1}</TableCell>
+                                            <TableCell className="flex items-center">
+                                                <Avatar className="mr-2">
+                                                    <AvatarImage src={score.avatarUrl} alt={score.username} />
+                                                    <AvatarFallback>{score.username.charAt(0)}</AvatarFallback>
+                                                </Avatar>
+                                                {score.username}
+                                            </TableCell>
+                                            <TableCell>{score.wins}</TableCell>
+                                            <TableCell>{score.draws}</TableCell>
+                                            <TableCell>{score.losses}</TableCell>
+                                            <TableCell>{score.score}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        )}
                     </TabsContent>
                     <TabsContent value="breakout">
                         {/* Répétez pour chaque onglet */}
